3cff96f402847959a696b3ba67bd1ac792f1ade0
sohanrambhagatttt81 <sohananuragi81@gmail>
2026-02-28 12:43:12 +0530

    Fix Image Path

--- Frontend/src/Pages/Home/HomeSwipper/Swipper.jsx
+++ Frontend/src/Pages/Home/HomeSwipper/Swipper.jsx
@@ -5,16 +5,16 @@ import "swiper/css";
 import { IoIosArrowBack } from "react-icons/io";
 import { MdArrowForwardIos } from "react-icons/md";
 
-import cat1 from "./swipperimg/cat_1.jpg";
-import cat2 from "./swipperimg/cat_2.jpg";
-import cat3 from "./swipperimg/cat_3.jpg";
-import cat4 from "./swipperimg/cat_4.jpg";
-import cat5 from "./swipperimg/cat_5.jpg";
-import cat6 from "./swipperimg/cat_6.jpg";
-import cat7 from "./swipperimg/cat_7.jpg";
-import cat8 from "./swipperimg/cat_8.jpg";
-import cat9 from "./swipperimg/cat_9.jpg";
-import cat10 from "./swipperimg/cat_10.jpg";
+import cat1 from "./swipperImg/cat_1.jpg";
+import cat2 from "./swipperImg/cat_2.jpg";
+import cat3 from "./swipperImg/cat_3.jpg";
+import cat4 from "./swipperImg/cat_4.jpg";
+import cat5 from "./swipperImg/cat_5.jpg";
+import cat6 from "./swipperImg/cat_6.jpg";
+import cat7 from "./swipperImg/cat_7.jpg";
+import cat8 from "./swipperImg/cat_8.jpg";
+import cat9 from "./swipperImg/cat_9.jpg";
+import cat10 from "./swipperImg/cat_10.jpg";
 
 const slides = [
   {
